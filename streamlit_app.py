dd3697cfe6d99c9056efffb0bc35e22a388172ce
Phạm Lâm Duy <143507583+LamDuy77777@users.noreply.github.com>
2025-02-26 14:59:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,5 @@ with st.expander('Data'):
 
 with st.expander('Data visualization'):
   st.write("### Distribution of pEC50")
-  st.bar_chart(data = df, x = 'pEC50', y = 'None')
+  st.bar_chart(data = df, x = 'pEC50', y = False)
 
