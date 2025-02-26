5d26fe15c2097b30ed36cd0da45113f4aadd175f
Phạm Lâm Duy <143507583+LamDuy77777@users.noreply.github.com>
2025-02-26 14:09:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,8 @@ import pandas as pd
 st.title('🤖 Machine Learning App 🤖')
 
 st.info('This app will help you predict the pEC50 of apelin receptor agonists.')
-df = pd.read_csv('https://raw.githubusercontent.com/LamDuy77777/data/refs/heads/main/Apelin_1715.csv')
-df
+
+with st.expander('Data'):
+  st.write('**Standardized data Apelin**')
+  df = pd.read_csv('https://raw.githubusercontent.com/LamDuy77777/data/refs/heads/main/Apelin_1715.csv')
+  df
